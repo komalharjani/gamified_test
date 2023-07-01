65a42af06d978d979917ea13866499c34a522ac3
komalharjani <komaalharjani@gmail.com>
2023-07-01 18:48:23 +0100

    added html
    Co-authored-by: Lys Irebe <lysirebe@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,15 @@
+const wordPairs = [
+    { word1: "happy", word2: "joyful", type: "s" },
+    { word1: "big", word2: "small", type: "a" },
+    { word1: "hot", word2: "cold", type: "a" },
+    { word1: "fast", word2: "slow", type: "a" },
+    { word1: "happy", word2: "sad", type: "a" },
+    { word1: "smart", word2: "stupid", type: "a" },
+    { word1: "angry", word2: "calm", type: "a" },
+    { word1: "joyful", word2: "sad", type: "n" },
+    { word1: "big", word2: "fast", type: "n" },
+    { word1: "small", word2: "stupid", type: "n" }
+  ];
+
+
+  
